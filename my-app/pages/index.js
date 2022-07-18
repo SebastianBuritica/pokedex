a91f2ea1fc54293b1a3800985a5494f9a5861d43
SebastianBuritica <93333842+SebastianBuritica@users.noreply.github.com>
2022-07-17 20:45:01 -0500

    Half way
    Nextjs set up, tailwind configured, currently in the process of fetching data from the pokemon api.

---
+++ my-app/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/Head'
+import Layout from "../components/Layout"
+
+
+export default function Home() {
+  return (
+    <Layout title='NextJS Pokedex'>    
+        <h1 className='text-4xl mb-8 text-center'>
+          NextJS Pokedex
+        </h1>
+    </Layout>
+  )
+}
