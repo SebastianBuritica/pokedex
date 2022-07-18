e8af6246a14f0724bd4e536e007624d67a162366
SebastianBuritica <93333842+SebastianBuritica@users.noreply.github.com>
2022-07-17 22:00:30 -0500

    Create pokemon.js

---
+++ my-app/pages/pokemon.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Link from 'next/Link'
+
+export default function pokemon({pokeman})  {
+  return (
+    <div>pokemon</div>
+  )
+}
+
+export default pokemon
